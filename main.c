6330cae7ba3a31982e4e2bdb95fa3543d15c84a8
Antony Hwang <AntonyHwang@hotmail.com>
2016-11-14 02:25:46 +0000

    cd fixed and fork fixed

--- main.c
+++ main.c
@@ -18,8 +18,8 @@
 
 #define MAX_BUFF_SIZE 1024
 
-char path[MAX_BUFF_SIZE];
-char home[MAX_BUFF_SIZE];
+char path[MAX_BUFF_SIZE] = "";
+char home[MAX_BUFF_SIZE] = "";
 
 char **paths;
 
@@ -29,6 +29,8 @@ int prefix(const char *pre, const char *str)
 }
 
 bool loadConfigFile() {
+    char *buffer;
+    buffer = malloc(sizeof(char) * MAX_BUFF_SIZE);
     FILE *pfile;
     char *filename = "profile";
     int pathAssigned = 0;
@@ -39,8 +41,6 @@ bool loadConfigFile() {
         exit(1);
     }
     while(1) {
-        char buffer[MAX_BUFF_SIZE];
-        
         fgets (buffer, MAX_BUFF_SIZE, pfile);
         if (prefix("PATH=", buffer)) {
             strncpy(path, buffer + 5, strlen(buffer) - 5);
@@ -56,6 +56,7 @@ bool loadConfigFile() {
                 return true;
             }
         }
+        free(buffer);
     }
     fclose(pfile);
 }
@@ -71,13 +72,48 @@ void getSearchPath() {
         counter++;
         token = strtok(NULL, "\n:");
     }
+    paths[counter] = " ";
+}
+
+void cd(char **arg) {
+    char cwd[MAX_BUFF_SIZE] = "";
+    if (strcmp(arg[1], " ") == 0) {
+        strcat(cwd, home);
+    }
+    else {
+        getcwd(cwd, sizeof(cwd));
+        strcat(cwd, "/");
+        strcat(cwd, arg[1]);
+    }
+    int status = chdir(cwd);
+    if (status != 0) {
+        printf("Failed to change directory\n");
+        printf("No such file or directory\n");
+    }
+}
+
+int launchCommand(char *buffer, char **command) {
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        printf("fork failed");
+    }
+    else if (pid > 0)
+    {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    else
+    {
+        execv(buffer, command);
+    }
+    return 1;
 }
 
 int runProgramArg() {
-    int found = 0;
     char *input = malloc( MAX_BUFF_SIZE * sizeof( char* ));
     char **command = malloc( MAX_BUFF_SIZE * sizeof( char* ));
-
     fgets(input, MAX_BUFF_SIZE, stdin);
     char *token;
     int counter = 0;
@@ -88,50 +124,45 @@ int runProgramArg() {
         counter++;
         token = strtok(NULL, " \n\t");
     }
-    for (int i = 0; i < MAX_BUFF_SIZE; i++) {
-        char *buffer;
-        buffer = malloc(sizeof(char) * MAX_BUFF_SIZE);
-        strcpy(buffer, paths[i]);
-        strcat(buffer, "/");
-        strcat(buffer, command[0]);
-        if (strlen(paths[i]) == 0) {
-            break;
-        }
-        
-        if (access(buffer, X_OK) == 0) {
-            printf("found\n");
-            if (fork() != 0) {
-                wait(NULL);
-            }
-            else {
-                execv(buffer, command);
-            }
-            found = 1;
-            free(buffer);
-            free(command);
-            free(input);
-            break;
-        }
-    }
-    if (found == 0) {
-        printf("command not found\n");
+    if (strcmp(command[0], "cd") == 0) {
+        command[counter] = " ";
+        cd(command);
         free(command);
         free(input);
+        return 1;
     }
-    if (fork() != 0) {
-        wait(NULL);
+    else {
+        for (int i = 0; i < MAX_BUFF_SIZE; i++) {
+            char buffer[MAX_BUFF_SIZE] = "";
+            strcpy(buffer, paths[i]);
+            strcat(buffer, "/");
+            strcat(buffer, command[0]);
+            if (access(buffer, X_OK) == 0) {
+                int status = launchCommand(buffer, command);
+                free(command);
+                free(input);
+                return status;
+            }
+            if (strcmp(paths[i], " ")) {
+                break;
+            }
+        }
     }
+    printf("command not found\n");
+    free(command);
+    free(input);
     return 1;
 }
 
 void runShell () {
-    bool commandInput = 0;
+
+    int commandInput = 0;
     getSearchPath();
     do {
+        char cwd[MAX_BUFF_SIZE] = "";
         commandInput = 0;
-        char cwd[MAX_BUFF_SIZE];
         if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            fprintf(stdout, "%s>", cwd);
+            printf("%s>", cwd);
         }
         else {
             perror("Error: current directory not found");
